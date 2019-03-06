a1dbb22d5f70ab86014d49b62d4b59d0f3c99dc6
Rayckey <wwang79@jhu.edu>
2019-03-06 13:20:18 +0800

    added two paras

--- src/xr1controllerol/xr1controllerol.cpp
+++ src/xr1controllerol/xr1controllerol.cpp
@@ -729,7 +729,7 @@ void XR1ControllerOL::broadcastTransform() {
 //    XR1_ptr->setInverseDynamicsOption(XR1::GravityCompensation);
 
     // This function triggers almost all the computation in the library
-    XR1_ptr->triggerCalculation(false);
+    XR1_ptr->triggerCalculation(true);
 
     // Publish the left one
     XR1_ptr->getEndEffectorTransformation(XR1::LeftArm, itsafine);
@@ -943,23 +943,23 @@ void XR1ControllerOL::clearStates() {
 void XR1ControllerOL::gravityCallback() {
 
 
-//    if (XR1_ptr->getControlMode(XR1::LeftArm) == XR1::ForceMode) {
-//        for (uint8_t i = XR1::LeftArm; i < XR1::Left_Wrist_Z; i++) {
-//            ROS_INFO("The Current for joint [%d] is [%f]", (int) i, XR1_ptr->getTargetJointCurrent(i));
+    if (XR1_ptr->getControlMode(XR1::LeftArm) == XR1::ForceMode) {
+        for (uint8_t i = XR1::LeftArm; i < XR1::Left_Wrist_Z; i++) {
+            ROS_INFO("The Current for joint [%d] is [%f]", (int) i, XR1_ptr->getTargetJointCurrent(i));
+
+            setJointCurrent(i, XR1_ptr->getTargetJointCurrent(i));
+
+        }
+    }
 //
-//            setJointCurrent(i, XR1_ptr->getTargetJointCurrent(i));
-//
-//        }
-//    }
-//
-//    if (XR1_ptr->getControlMode(XR1::RightArm) == XR1::ForceMode) {
-//        for (uint8_t i = XR1::RightArm; i < XR1::Right_Wrist_Z; i++) {
-//            ROS_INFO("The Current for joint [%d] is [%f]", (int) i, XR1_ptr->getTargetJointCurrent(i));
-//
-//            setJointCurrent(i, XR1_ptr->getTargetJointCurrent(i));
-//
-//        }
-//    }
+    if (XR1_ptr->getControlMode(XR1::RightArm) == XR1::ForceMode) {
+        for (uint8_t i = XR1::RightArm; i < XR1::Right_Wrist_Z; i++) {
+            ROS_INFO("The Current for joint [%d] is [%f]", (int) i, XR1_ptr->getTargetJointCurrent(i));
+
+            setJointCurrent(i, XR1_ptr->getTargetJointCurrent(i));
+
+        }
+    }
 
 
 }
