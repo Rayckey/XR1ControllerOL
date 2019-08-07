d92cdacc15c5e051f65dac7726059f346357dafe
hopps.li <hopps.li@cloudminds.com>
2019-08-07 13:56:22 +0800

    rename jointStatesTable in hopps.cpp
    Change-Id: I27653675ca950994f1e444e5da9ad55fc3700d20

--- src/xr1controllerol/xr1controllerolhopps.cpp
+++ src/xr1controllerol/xr1controllerolhopps.cpp
@@ -7,9 +7,9 @@
 
 void XR1ControllerOL::setupJointStateTable(){
 
-    m_jointlookup["Wheel_Left"] = 1;
-    m_jointlookup["Wheel_Right"] = 2;
-    m_jointlookup["Wheel_Back"] = 3;
+    m_jointlookup["Wheel_left"] = 1;
+    m_jointlookup["Wheel_right"] = 2;
+    m_jointlookup["Wheel_back"] = 3;
     m_jointlookup["Knee_X"] = 4;
     m_jointlookup["Back_Z"] = 5;
     m_jointlookup["Back_X"] = 6;
@@ -31,21 +31,21 @@ void XR1ControllerOL::setupJointStateTable(){
     m_jointlookup["Right_Wrist_Z"] = 22;
     m_jointlookup["Right_Wrist_X"] = 23;
     m_jointlookup["Right_Wrist_Y"] = 24;
-    m_jointlookup["Left_Thumb"] = 25;
-    m_jointlookup["Left_Index"] = 26;
-    m_jointlookup["Left_Middle"] = 27;
-    m_jointlookup["Left_Ring"] = 28;
-    m_jointlookup["Left_Pinky"] = 29;
-    m_jointlookup["Right_Thumb"] = 30;
-    m_jointlookup["Right_Index"] = 31;
-    m_jointlookup["Right_Middle"] = 32;
-    m_jointlookup["Right_Ring"] = 33;
-    m_jointlookup["Right_Pinky"] = 34;
+    m_jointlookup["Left_1_1"] = 25;
+    m_jointlookup["Left_2_1"] = 26;
+    m_jointlookup["Left_3_1"] = 27;
+    m_jointlookup["Left_4_1"] = 28;
+    m_jointlookup["Left_5_1"] = 29;
+    m_jointlookup["Right_1_1"] = 30;
+    m_jointlookup["Right_2_1"] = 31;
+    m_jointlookup["Right_3_1"] = 32;
+    m_jointlookup["Right_4_1"] = 33;
+    m_jointlookup["Right_5_1"] = 34;
 
 
-    m_namelookup[1] = "Left_Front";
-    m_namelookup[2] = "Right_Front";
-    m_namelookup[3] = "Back_Wheel";
+    m_namelookup[1] = "Wheel_left";
+    m_namelookup[2] = "Wheel_right";
+    m_namelookup[3] = "Wheel_back";
     m_namelookup[4] = "Knee_X";
     m_namelookup[5] = "Back_Z";
     m_namelookup[6] = "Back_X";
@@ -67,16 +67,16 @@ void XR1ControllerOL::setupJointStateTable(){
     m_namelookup[22] = "Right_Wrist_Z" ;
     m_namelookup[23] = "Right_Wrist_X" ;
     m_namelookup[24] = "Right_Wrist_Y" ;
-    m_namelookup[25] = "Left_Thumb" ;
-    m_namelookup[26] = "Left_Index" ;
-    m_namelookup[27] = "Left_Middle" ;
-    m_namelookup[28] = "Left_Ring" ;
-    m_namelookup[29] = "Left_Pinky" ;
-    m_namelookup[30] = "Right_Thumb" ;
-    m_namelookup[31] = "Right_Index" ;
-    m_namelookup[32] = "Right_Middle" ;
-    m_namelookup[33] = "Right_Ring" ;
-    m_namelookup[34] = "Right_Pinky" ;
+    m_namelookup[25] = "Left_1_1" ;
+    m_namelookup[26] = "Left_2_1" ;
+    m_namelookup[27] = "Left_3_1" ;
+    m_namelookup[28] = "Left_4_1" ;
+    m_namelookup[29] = "Left_5_1" ;
+    m_namelookup[30] = "Right_1_1" ;
+    m_namelookup[31] = "Right_2_1" ;
+    m_namelookup[32] = "Right_3_1" ;
+    m_namelookup[33] = "Right_4_1" ;
+    m_namelookup[34] = "Right_5_1" ;
 
     for (uint8_t fake_joint_id = 1; fake_joint_id <= 34 ; fake_joint_id++){
 
